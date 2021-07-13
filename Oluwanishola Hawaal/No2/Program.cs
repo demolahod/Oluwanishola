95b2b9b04435d6ce1d6ccfeb2dc6a4ada2e4b0d1
demolahod <87329584+demolahod@users.noreply.github.com>
2021-07-13 10:19:21 +0100

    Add files via upload

---
+++ Oluwanishola Hawaal/No2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace No2StudentVerification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pls enter your program");
+            string userInput = Console.ReadLine().ToLower();
+            if (userInput == "full time"){
+                Console.WriteLine("Enter your course");
+                Console.Write(Console.ReadLine() + " is a good course");
+            } else if (userInput == "part time"){
+                Console.WriteLine("How many credit are you taking?");
+                int UserCreditNo = int.Parse(Console.ReadLine());
+                if (UserCreditNo > 6) {
+                    Console.WriteLine("Too much unit for a part-time student");
+                    Console.WriteLine("you are allow to enter maximum of 6 credit, TRY AGAIN!!!");
+                    UserCreditNo = int.Parse(Console.ReadLine());
+                    if (UserCreditNo <= 6) {
+                        Console.WriteLine("proceed to screen");
+                    } else {
+                        Console.WriteLine("You enter the wrong input");
+                    }
+
+                } else {
+                    Console.WriteLine("proceed to screen");
+                }
+
+            }
+        }
+    }
+}
